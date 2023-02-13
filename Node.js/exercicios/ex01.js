9909e8b8fa243a3417ac0e329db9288988d6a5ac
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-02-13 10:35:13 -0300

    Add readline ex01

--- Node.js/exercicios/ex01.js
+++ Node.js/exercicios/ex01.js
@@ -1,7 +1,21 @@
 // crie um programa que leia dois numeros de usuarios e exiba o resultado da soma entre eles.
 
-let a = 2;
-let b = 3; 
-let soma = a + b;
+// let a = 2;
+// let b = 3; 
+// let soma = a + b;
 
-console.log(soma);
+// console.log(soma);
+
+
+let a;
+let b;
+let soma;
+
+let readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+     readline.question('adicione o primeiro valor: ', a => 
+     readline.question('adicione o segundo valor: ', b =>    
+     console.log(parseInt(a)+parseInt(b))));
+     readline.close();
