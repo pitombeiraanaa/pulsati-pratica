55449ae967b895952fb3630d43a8620189b2f78b
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-02-14 09:19:28 -0300

    Add exercicios e resoluções

--- Node.js/exercicios-node/ex15.js
+++ Node.js/exercicios-node/ex15.js
@@ -1,2 +1,13 @@
 // crie um programa que leia uma string do usuario e verifique se é palindromo
 
+let palavra;
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('Digite uma palavra ', palavra=> {
+  console.log(palavra.toLowerCase() === palavra.split('').reverse().join(''));
+  readline.close(); 
+});
---
+++ Node.js/exercicios-node/ex16.js
@@ -0,0 +1,11 @@
+//crie um programa que leia a string de um usuário e as exiba em ordem alfabetica
+
+let palavra;
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('Digite a primeira palavra ', palavra1 => {
+  });
