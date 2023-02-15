eaa214b09611f75501708a523ec3b12b8cbbee63
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-02-15 13:59:13 -0300

    Add método GET aos passageiros

--- Node.js/exercicioEndPoint/serverPassageiros.js
+++ Node.js/exercicioEndPoint/serverPassageiros.js
@@ -1,66 +1,66 @@
-const http = require('http');
-const classes = require('./classes').Passageiros;
-const salvarArquivo = require('./arquivo').salvarArquivo;
-const lerArquivo = require('./arquivo').lerArquivo;
+const http = require("http");
+const classes = require("./classes").Passageiros;
+const salvarArquivo = require("./arquivo").salvarArquivo;
+const lerArquivo = require("./arquivo").lerArquivo;
 
-http.createServer((req, res) => {
-    if (req.method == 'POST') {
-      if (req.url.indexOf('/Passageiros') >= 0) {
+http
+  .createServer((req, res) => {
+
+    //METODO POST
+    if (req.method == "POST") {
+      if (req.url.indexOf("/Passageiros") >= 0) {
         var body = "";
-        req.on('data', function (chunk) {
+        req.on("data", function (chunk) {
           body += chunk;
         });
-        req.on('end', function () {
+
+        req.on("end", function () {
           const Passageiros = JSON.parse(body);
           console.log(Passageiros);
-          
-          salvarArquivo(`${Passageiros.nomePassageiro}.json`, JSON.stringify(Passageiros));
-          res.writeHead(201, {'Content-Type': 'application/json'});
-          res.end(JSON.stringify({data: "Criado com Sucesso"}));
-        });
-      } else {
-        res.end('Not found');
-      } 
 
-
-
-    } else if (req.method == 'PUT') {
-      if (req.url.indexOf('/Passageiros') >= 0) {
-        var body = "";
-        req.on('data', function (chunk) {
-          body += chunk;
+          salvarArquivo(
+            `${Passageiros.nomePassageiro}.json`,
+            JSON.stringify(Passageiros)
+          );
+          res.writeHead(201, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ data: "Criado com Sucesso" }));
         });
-        req.on('end', function () {
-          const Passageiros = JSON.parse(body);
-          console.log(Passageiros);
-          salvarArquivo(`${Passageiros.nomePassageiro}.json`, JSON.stringify(Passageiros));
-          res.writeHead(202, {'Content-Type': 'application/json'});
-          res.end(JSON.stringify({data: "Atualizado com Sucesso"}));
-        });
-      } else {
-        res.end('Not found');
       }
 
+      //METODO GET
+    } else if (req.method == "GET") {
+      if (req.url.indexOf("/Passageiros") >= 0) {
+        res.writeHead(201, {
+          "Content-Type": "application/json",
+        });
 
-
-    } else if(req.method == 'GET')
-    if (req.url.indexOf('/Passageiros') >= 0) {
-      var body = "";
-      req.on('data', function (chunk) {
-        body += chunk;
-      });
-      req.on('end', function () {
-        const Passageiros = JSON.parse(body);
-        console.log(Passageiros);
-        salvarArquivo(`${Passageiros.nomePassageiros}.json`, JSON.stringify(Passageiros));
-        res.writeHead(203, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({data: "Listado com sucesso"}));
-      });
+        lerArquivo("./Passageiros.json").then((conteudo) => {
+          res.end(conteudo);
+        });
+      } else {
+        res.end("Not found");
+      }
     } else {
-      res.end('Not found');
+      res.end("Not found");
     }
+  })
+  .listen(8000, () => console.log("Servidor inicializado na porta 8000"));
 
-  }).listen(8000, () => console.log('Servidor inicializado na porta 8000'));
 
 
-  
+  // } else if (req.method == 'PUT') {
+    //   if (req.url.indexOf('/Passageiros') >= 0) {
+    //     var body = "";
+    //     req.on('data', function (chunk) {
+    //       body += chunk;
+    //     });
+    //     req.on('end', function () {
+    //       const Passageiros = JSON.parse(body);
+    //       console.log(Passageiros);
+    //       salvarArquivo(`${Passageiros.nomePassageiro}.json`, JSON.stringify(Passageiros));
+    //       res.writeHead(202, {'Content-Type': 'application/json'});
+    //       res.end(JSON.stringify({data: "Atualizado com Sucesso"}));
+    //     });
+    //   } else {
+    //     res.end('Not found');
+    //   }
