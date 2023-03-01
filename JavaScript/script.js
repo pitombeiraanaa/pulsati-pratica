9e4ef7e5c8f7455ce00a584b851d7663c0b53926
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-03-01 08:15:16 -0300

    Add javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+// verificar o tipo de variavel
+let nome = 'Ana';
+console.log(typeof nome);
+
+
+//conversões de tipo 
+let numero1 = "1";
+let numero2 = 2;
+
+parseInt(numero1);
+parseFloat(numero1); // converte para number mantendo as casas decimais
+String(numero2) //converte para string
+Number(numero1) //converte para number
