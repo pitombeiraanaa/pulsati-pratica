d4ba55c978dc076082978b20bb4abe2b88859f36
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-03-03 17:14:12 -0300

    Add projeto finanças

---
+++ Proj. Finanças/script.js
@@ -0,0 +1,14 @@
+const Modal = {
+    open(){
+        // abrir modal
+        //adicionar a class active ao modal
+       document.querySelector('.modal-overlay').classList.add('active')
+
+    },
+    close(){
+       //fechar modal
+       //remover a class active do modal
+       document.querySelector('.modal-overlay').classList.remove('active')
+     }
+}
+
