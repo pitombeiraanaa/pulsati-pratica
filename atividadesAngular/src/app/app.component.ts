b9e4b1acad3163db8677d60a78deca10ba6edbe1
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2023-03-16 17:39:44 -0300

    Add modificações no proj mercado livre

--- atividadesAngular/src/app/app.component.ts
+++ atividadesAngular/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'atividadesAngular';
+  title = 'mercadoLivre';
+
+  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
 }
--- atividadesAngular/src/app/app.module.ts
+++ atividadesAngular/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MercadoLivreComponent } from './pages/mercado-livre/mercado-livre.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -17,8 +23,13 @@ import {MatTabsModule} from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatTabsModule,
     MatToolbarModule,
-    MatTabsModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
